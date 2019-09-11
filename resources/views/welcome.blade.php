3dbfd2b00f8822a5a38ddd434ff6afaeee4b426e
2225373005 <1003014125@qq.com>
2019-09-11 15:46:21 +0800

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    儿子你来了
+                    来了老弟
                 </div>
 
                 <div class="links">
